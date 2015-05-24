dd56d81e7107094cb26ab20f3732e3c693fdfe99
Matthew Canty <matthewcanty@gmail.com>
2015-05-24 20:34:51 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# scores
-All my scores
+# Prerequisites
+## MuseScore
+Add `mscore cli` to `PATH` 
+### Mac OS
+`export PATH="/Applications/MuseScore 2.app/Contents/MacOS/":$PATH`
+# Getting Started
+### Generate all scores
+`sh generate.sh /`
+### Generate all Madness scores
+`sh generate.sh /madness`
+
--- generate.sh
+++ generate.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-case "$OSTYPE" in
-  solaris*) echo "SOLARIS" ;;
-  darwin*)  mscore="/Applications/MuseScore 2.app/Contents/MacOS/mscore" ;; 
-  linux*)   echo "LINUX" ;;
-  bsd*)     echo "BSD" ;;
-  *)        echo "unknown: $OSTYPE" ;;
-esac
-
 find src/$1 -name '*.xml' | while read line; do
 
 	outputFile="${line/.xml/.pdf}"
@@ -17,7 +8,7 @@ find src/$1 -name '*.xml' | while read line; do
 
     echo "Processing '${line##*/}'"
 
-    "${mscore}" $line -o $outputFile
+    mscore $line -o $outputFile
     
     echo "Generated '${outputFile##*/}'"
 done
